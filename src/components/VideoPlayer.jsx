b4b870cc6c104022d8bab8ba1f9fdd34bf14d07a
Kevin Le <iamkevinvle@gmail.com>
2023-01-04 11:09:38 -0800

    display currently playing video

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,13 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item"
+      src = {`https://www.youtube.com/embed/${props.currentPlaying.id.videoId}?autoplay=1`}
+       allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.currentPlaying.snippet.title}</h3>
+      <div>{props.currentPlaying.snippet.description}</div>
     </div>
   </div>
 );
@@ -19,3 +21,8 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default VideoPlayer;
+
+// src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+
+// src = {`https://www.youtube.com/embed/${props.currentPlaying.id.videoId}?autoplay=1`}
+//        allowFullScreen></iframe>
