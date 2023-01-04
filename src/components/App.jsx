949724e766f9bcf3bf8e2b4d6d5213ab9e272437
Kevin Le <iamkevinvle@gmail.com>
2023-01-04 11:07:09 -0800

    render child components on app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,33 @@
 import exampleVideoData from '../data/exampleVideoData.js'
+import Search from './Search.js'
+import VideoList from './VideoList.js'
+import VideoPlayer from './VideoPlayer.js'
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+// create clone of imported exampleVideoData as state
+var App = () => {
+  const [videos, setVideos] = React.useState([...exampleVideoData])
+
+  // // create current video state
+  const [currentPlaying, setCurrentPlaying] = React.useState(exampleVideoData[0])
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search />
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer currentPlaying={currentPlaying}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={videos}/>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
